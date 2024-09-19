9b6ce0054fe0bb56df0cec1be50f06a6afc23314
EmmaBrown17 <emmabrow@andrew.cmu.edu>
2024-09-19 19:08:44 -0400

    working my way pt 1

---
+++ solution-hw3/js/app.js
@@ -0,0 +1,79 @@
+/* for product description page:
+
+from glazing id, 
+
+if keep original add 0
+if sugar milk add 0
+if vanilla milk add 0.50
+if double chocolate add 1.50
+to total 
+
+from pack size id,
+
+if one * 1
+if 3 * 3
+if 6 * 5
+if 12 * 10
+*/
+
+let basePrice = 2.49;
+
+let allGlazing = [
+    {
+        glazing: 'keep-original',
+        price: (basePrice + 0),
+    },
+    {
+        glazing: 'sugar-milk',
+        price: (basePrice + 0),
+    },
+    {
+        glazing: 'vanilla-milk',
+        price: (basePrice + 0.50),
+    },
+    {
+        glazing: 'double-chocolate',
+        price: (basePrice + 1.50),
+    },
+];
+
+let allPackSize = [
+    {
+        packsize: 'keep-original',
+        price: (basePrice * 1),
+    },
+    {
+        packsize: 'sugar-milk',
+        price: (basePrice * 3),
+    },
+    {
+        packsize: 'vanilla-milk',
+        price: (basePrice * 5),
+    },
+    {
+        packsize: 'double-chocolate',
+        price: (basePrice * 10),
+    },
+]
+
+/*
+function displayPrice(glaze) {
+    let glazeElement = document.querySelector('#price-info');
+
+    glazeElement.innerText = glaze.price;
+}
+    */
+
+function glazingChange(element){
+    console.log("hi");
+
+    let glazeID = document.getElementById('glazing');
+    let value = glazeID.options[glazeID.selectedIndex].text;
+    console.log("You chose " + value);
+
+
+    // get value of glaze option
+    const priceChange = element.value;
+    //update the price
+}
+

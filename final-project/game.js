2efa096a502f84ab3480117f037176e0b69ae91a
EmmaBrown17 <emmabrow@andrew.cmu.edu>
2024-11-17 01:09:47 -0500

    working on creating the puzzle structure

---
+++ final-project/game.js
@@ -0,0 +1,132 @@
+let gridSize = 5; 
+let cellSize = 80; 
+let gridState = []; 
+let rowClues  = [[3], [1, 1], [5], [1, 1], [3]];
+let colClues = [[1], [5], [1, 1, 1], [5], [1]];
+let solution = [
+  [0, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0],
+  [1, 1, 1, 1, 1],
+  [0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0],
+];
+let lives = 3;
+
+function setup() {
+  createCanvas(gridSize * cellSize + 100, gridSize * cellSize + 100);
+
+  for (let i = 0; i < gridSize; i++) {
+    gridState[i] = Array(gridSize).fill(0);
+  }
+}
+
+function draw() {
+  background(255);
+  drawGrid();
+  drawClues();
+  displayLives();
+  checkGameOver();
+}
+
+function drawGrid() {
+  for (let row = 0; row < gridSize; row++) {
+    for (let col = 0; col < gridSize; col++) {
+      let x = col * cellSize + 100;
+      let y = row * cellSize + 100;
+
+      fill(gridState[row][col] === 1 ? 'black' : 'white');
+      stroke('black');
+      rect(x, y, cellSize, cellSize);
+    }
+  }
+}
+
+function drawClues() {
+    for (let i = 0; i < gridSize; i++) {
+        let clueText = rowClues[i].join(' ');
+        fill(0);
+        textSize(18);
+        textAlign(RIGHT, CENTER);
+        text(clueText, 90, 100 + i * cellSize + cellSize / 2);
+    }
+
+    for (let i = 0; i < gridSize; i++) {
+        let clueText = colClues[i].join(' ');
+        fill(0);
+        textSize(18);
+        textAlign(CENTER, BOTTOM);
+        text(clueText, 100 + i * cellSize + cellSize / 2, 90);
+    }
+}
+
+function mousePressed() {
+  let col = floor((mouseX - 100) / cellSize);
+  let row = floor((mouseY - 100) / cellSize);
+
+  if (row >= 0 && row < gridSize && col >= 0 && col < gridSize) {
+    gridState[row][col] = gridState[row][col] === 0 ? 1 : 0;
+    checkSolution();
+  }
+}
+
+// function checkGuess() {
+//     if (!arraysEqual(gridState, solution)) {
+//         lives--;
+//         console.log("Wrong guess! Lives left: " + lives);
+//     }
+// }
+
+// function arraysEqual(arr1, arr2) {
+//     for (let i = 0; i < arr1.length; i++) {
+//         for (let j = 0; j < arr1[i].length; j++) {
+//             if (arr1[i][j] !== arr2[i][j]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+function checkSolution() {
+  let result = document.getElementById('result');
+  if (arraysEqual(gridState, solution)) {
+    result.textContent = 'Correct Solution!';
+    result.style.color = 'green';
+  } else {
+    result.textContent = 'Incorrect Solution.';
+    result.style.color = 'red';
+    lives--;
+    console.log("Wrong guess! Lives left: " + lives);
+  }
+}
+
+function arraysEqual(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[i].length; j++) {
+      if (arr1[i][j] !== arr2[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function displayLives() {
+    fill(0);
+    textSize(20);
+    text('Lives: ' + lives, 10, height - 10);
+}
+
+function checkGameOver() {
+    if (lives <= 0) {
+        let result = document.getElementById('result');
+        result.textContent = 'Game Over! You ran out of lives.';
+        result.style.color = 'red';
+        noLoop();
+    } else if (arraysEqual(gridState, solution)) {
+        let result = document.getElementById('result');
+        result.textContent = 'You solved it!';
+        result.style.color = 'green';
+        noLoop();
+    }
+}
